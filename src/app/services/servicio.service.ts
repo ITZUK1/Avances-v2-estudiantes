b9fd819af13120dba1fc40d52870c1d575b87c62
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-10-10 14:16:21 -0500

    se creo el servicio y se mejoro el css

---
+++ src/app/services/servicio.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  // Cambia esta URL a la de tu API
+  private apiUrl = 'http://tuservidor.com/api/login';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, id: string): Observable<any> {
+    return this.http.post<any>(this.apiUrl, { email, id });
+  }
+}
