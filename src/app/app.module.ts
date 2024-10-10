753b25a9de06a9ab9685d73d8254b04017ca2092
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-10-10 14:06:21 -0500

    se muestra el login de la pantalla principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PantallaInicioComponent } from './components/pantalla-inicio/pantalla-inicio.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { PantallaInicioComponent } from './components/pantalla-inicio/pantalla-i
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
