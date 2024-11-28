859c619d718f2ea674156527cb450affc37d581d
MIGUEL ANGEL BEJARANO VERDUGO <128190067+ITZUK1@users.noreply.github.com>
2024-11-28 09:58:15 -0500

    se creo la inasistencia y tambien el html y css

--- src/app/components/pantalla-profesor/pantalla-profesor.component.ts
+++ src/app/components/pantalla-profesor/pantalla-profesor.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; // Importar HttpClient
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'pantalla-profesor',
@@ -18,6 +18,15 @@ export class pantallProfesorComponent {
   };
   estadoNuevaMateria: string = '';
   errorMateria: string = '';
+  
+  // Variables para inasistencia
+  mostrarFormularioInasistencia: boolean = false;
+  inasistencia = {
+    fecha: '',
+    estudiante_id: null,
+    materia_id: null,
+    motivo: ''
+  };
 
   constructor(private http: HttpClient) {}
 
@@ -31,11 +40,28 @@ export class pantallProfesorComponent {
     this.http.post('/api/materia', this.nuevaMateria)
       .subscribe(
         (response: any) => {
-          this.estadoNuevaMateria = "Materia agregada con éxito con ID: " + response.id;
           this.nuevaMateria = { nombre: '', descripcion: '', semestre: '', fecha_inicio: '', status: 'activo', profesor_id: null, curso_id: null };
         },
         error => {
           this.errorMateria = "Error al agregar la materia: " + error.error.error;
+        }
+      );
+  }
+
+  toggleInasistenciaForm() {
+    this.mostrarFormularioInasistencia = !this.mostrarFormularioInasistencia;
+  }
+
+  agregarInasistencia() {
+    this.http.post('/api/inasistencia', this.inasistencia)
+      .subscribe(
+        (response: any) => {
+          alert("Inasistencia agregada con éxito!");
+          this.inasistencia = { fecha: '', estudiante_id: null, materia_id: null, motivo: '' };
+          this.toggleInasistenciaForm();  // Cierra el formulario después de agregar
+        },
+        error => {
+          alert("Error al agregar la inasistencia: " + error.error.error);
         }
       );
   }
